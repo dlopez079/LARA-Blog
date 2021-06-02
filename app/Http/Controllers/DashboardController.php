62f0ef335ad43579e560a319be1d4c2a3b314aff
dlopez079 <dlopez@mattmen.com>
2021-06-01 20:35:21 -0400

    Installed Laravel Breeze.  Configured different users accounts and roles. Created migrations necessary for the application.

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class DashboardController extends Controller
+{
+
+    // ---------------------------------------------
+    // This is the index.
+    // It will check the roles of the user
+    // and forward them tot he appropriate page. 
+    // There are only 3 roles currently on application
+    // ---------------------------------------------
+    public function index()
+    {
+        if(Auth::user()->hasRole('user')) {
+            return view('userdash');
+        } elseif(Auth::user()->hasRole('blogwriter')) {
+            return view('blogwriterdash');
+        } elseif(Auth::user()->hasRole('admin')) {
+            return view('dashboard');
+        }
+    }
+
+    public function myprofile()
+    {
+        return view('myprofile');
+    }
+}

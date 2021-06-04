43e1f6d3f0fb3cca895e19238911f7c0bb427cef
dlopez079 <dlopez@mattmen.com>
2021-06-04 02:22:22 -0400

    Compiled Laravel Mix.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,44 +9,53 @@
 
  
 <body class="width:%100 text-center">
-    
-    <nav class="bg-teal-500">
-        Social Media Nav
-    </nav>
-    
-    <header>
-        <div>Title</div>
-    </header>
 
-    <nav>
-        <div>Site Nav</div>
-    </nav>
 
-    <section>
-        
-        <div>
-            <h1>Blog List</h1>
-        </div>
-        
-        <!------------------------------------------------------------------------------------------------------->
-        <!-- Blog List Card -->
-        
-        <div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
-            <div class="md:flex">
-              <div class="md:flex-shrink-0">
-                <img class="h-48 w-full object-cover md:h-full md:w-48" src="https://via.placeholder.com/150" alt="Man looking at item at a store">
-              </div>
-              <div class="p-8">
-                <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Blog No ###</div>
-                <a href="#" class="block mt-1 text-lg leading-tight font-medium text-black hover:underline">Blog Title</a>
-                <p class="mt-2 text-gray-500">Getting a new business off the ground is a lot of hard work. Here are five ideas you can use to find your first customers.</p>
-              </div>
+    <div class="container">
+
+
+        <nav>
+            <div class="border-3 border-black bg-gray-300">
+                <div class="text-red-300 p-1.5">Title</div>
             </div>
-        </div>
+        </nav>
         
-        <!-------------------------------------------------------------------------------------------------------->
-        
-    </section>
+        <header>
+            <div class="text-gray-400">Title</div>
+        </header>
+    
+        <nav>
+            <div>Site Nav</div>
+        </nav>
+    
+        <section>
+            
+            <div>
+                <h1>Blog List</h1>
+            </div>
+            
+            <!------------------------------------------------------------------------------------------------------->
+            <!-- Blog List Card -->
+            
+            <div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
+                <div class="md:flex">
+                  <div class="md:flex-shrink-0">
+                    <img class="h-48 w-full object-cover md:h-full md:w-48" src="https://via.placeholder.com/150" alt="Man looking at item at a store">
+                  </div>
+                  <div class="p-8">
+                    <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Blog No ###</div>
+                    <a href="#" class="block mt-1 text-lg leading-tight font-medium text-black hover:underline">Blog Title</a>
+                    <p class="mt-2 text-gray-500">Getting a new business off the ground is a lot of hard work. Here are five ideas you can use to find your first customers.</p>
+                  </div>
+                </div>
+            </div>
+            
+            <!-------------------------------------------------------------------------------------------------------->
+            
+        </section>
+
+    </div>
+    
 
 </body>
 </html>

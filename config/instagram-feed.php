45bb36f27f082119f22c3cab659da96751f7cf3c
David Lopez <dlopez@mattmen.com>
2021-06-18 15:08:56 -0400

    I added the Instagram keys to the Instagram feed configuration file.  I did not use the exact keys.  I used a method that will fetch the keys from the environment file.

--- config/instagram-feed.php
+++ config/instagram-feed.php
@@ -4,13 +4,13 @@ return [
     /*
      * The client_id from registering your app on Instagram
      */
-    'client_id'           => 'YOUR INSTAGRAM CLIENT ID',
+    'client_id'           => env($key, 'INSTAGRAM_CLIENT_ID'),
 
     /*
      * The client secret from registering your app on Instagram,
      * This is not the same as an access token.
      */
-    'client_secret'       => 'YOUR INSTAGRAM CLIENT SECRET',
+    'client_secret'       => env($key, 'INSTAGRAM_CLIENT_SECRET'),
 
     /*
      * The base url used to generate to auth callback route for instagram.
